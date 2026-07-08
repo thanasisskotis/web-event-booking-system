b90b099eff2e18f83b3ad6e585fafceda95a1d64
thanasisskotis <146757769+thanasisskotis@users.noreply.github.com>
2026-07-08 15:50:21 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
 
 # Connection string: postgresql://<user>:<password>@<host>/<database>
 engine = create_engine(settings.database_url)

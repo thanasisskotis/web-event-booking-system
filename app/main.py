255c7030b424ef17e69e59e398173675fa1092a0
thanasisskotis <146757769+thanasisskotis@users.noreply.github.com>
2026-07-08 23:56:06 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-from app.routers import admin, auth, events
+from app.routers import admin, auth, events, bookings
 
 app = FastAPI(title="Event Booking System")
 
 app.include_router(auth.router)
 app.include_router(admin.router)
 app.include_router(events.router)
+app.include_router(bookings.router)
 
 
 @app.get("/health")
